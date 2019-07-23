63cbd70a204c359fa0d27684a3fd12827d0de3be
Chau Huynh Ngoc <chauhn@phuthotech.vn>
2019-07-23 14:53:04 +0700

    init source

---
+++ app/src/main/java/vn/ptt/androidjetpack/DataBinding.java
@@ -0,0 +1,37 @@
+package vn.ptt.androidjetpack;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.UUID;
+
+import vn.ptt.androidjetpack.databinding.ActivityMainBinding;
+
+public class DataBinding extends AppCompatActivity {
+    private User user;
+    private int count = 0;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        user = new User();
+        user.setName("Ravi Tamada");
+        user.setEmail("ravi@androidhive.info");
+
+        binding.setUser(user);
+
+
+    }
+
+    public void click(View view) {
+        count++;
+        user.setName(user.getName() + count);
+        binding.setUser(user);
+    }
+}
