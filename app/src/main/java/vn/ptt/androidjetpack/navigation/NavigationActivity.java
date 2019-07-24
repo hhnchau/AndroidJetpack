0b858e1718fb5bdcb26feca949235aa659e5d81e
Chau Huynh Ngoc <chauhn@phuthotech.vn>
2019-07-24 15:54:16 +0700

    add menu choose

---
+++ app/src/main/java/vn/ptt/androidjetpack/navigation/NavigationActivity.java
@@ -0,0 +1,15 @@
+package vn.ptt.androidjetpack.navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.ptt.androidjetpack.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+    }
+}
---
+++ app/src/main/java/vn/ptt/androidjetpack/paging/PagingActivity.java
@@ -0,0 +1,15 @@
+package vn.ptt.androidjetpack.paging;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.ptt.androidjetpack.R;
+
+public class PagingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paging);
+    }
+}
