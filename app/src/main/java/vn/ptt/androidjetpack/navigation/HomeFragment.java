c07c57190df927cc41ce3335c1b76a35a262c44d
ChauHuynh-PC\Chau Huynh <hhnchau@gmail.com>
2019-07-24 23:36:21 +0700

    add navigation

---
+++ app/src/main/java/vn/ptt/androidjetpack/navigation/HomeFragment.java
@@ -0,0 +1,40 @@
+package vn.ptt.androidjetpack.navigation;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.navigation.Navigation;
+
+import vn.ptt.androidjetpack.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.btn).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_firstFragment));
+    }
+}
